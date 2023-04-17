a07af503cd68ba161c48bcca8ceb6d95eba37d73
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-04-17 17:58:25 +0200

    lista chat aperte

--- Milestone 1/js/script.js
+++ Milestone 1/js/script.js
@@ -1,11 +1,37 @@
-
 const {createApp} = Vue;
 
 createApp({
 
 
-  Data(){
+  data(){
     return{
+      contatti: [
+        {
+          name: 'Luca',
+          avatar: './img/avatar_1.jpg',
+          visible: true,
+        },
+        {
+          name: 'Marco',
+          avatar: './img/avatar_2.jpg',
+          visible: true,
+        },
+        {
+          name: 'Luigi',
+          avatar: './img/avatar_3.jpg',
+          visible: true,
+        },
+        {
+          name: 'Giuseppe',
+          avatar: './img/avatar_4.jpg',
+          visible: true,
+        },
+        {
+          name: 'Matteo',
+          avatar: './img/avatar_5.jpg',
+          visible: true,
+        }
+      ]
 
     }
   },
@@ -15,7 +41,7 @@ createApp({
   methods:{
 
     
-  }
+  },
 
 
 
